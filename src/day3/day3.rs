8671e66e378bce01c7062be37e7c94e416495632
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2025-04-09 18:22:16 +0200

    feat: upload second part of day 3

--- src/day3/day3.rs
+++ src/day3/day3.rs
@@ -15,10 +15,12 @@ fn main() -> io::Result<()> {
             }
         }
     }
-
+    for m in &marks {
+        println!("{}", m);
+    }
     let content_len = content.len();
     for i in 0..marks.len() {
-        if marks[i] > content_len / 2 {
+        if marks[i] >= content_len / 2 {
             marks[i] = 1;
         } else {
             marks[i] = 0;
@@ -36,6 +38,64 @@ fn main() -> io::Result<()> {
         }
     }
     let result1 = gamma * epsilon;
+
+    let mut oxigen = 0;
+    let mut co2 = 0;
+    let mut oxigen_vec = content.clone();
+    let mut co2_vec = content.clone();
+
+    let bit_len = oxigen_vec[0].len();
+    for i in 0..bit_len {
+        if oxigen_vec.len() == 1 {
+            break;
+        }
+        let ones = oxigen_vec
+            .iter()
+            .filter(|s| s.chars().nth(i) == Some('1'))
+            .count();
+        let keep = if ones * 2 >= oxigen_vec.len() {
+            '1'
+        } else {
+            '0'
+        };
+        oxigen_vec = oxigen_vec
+            .into_iter()
+            .filter(|s| s.chars().nth(i) == Some(keep))
+            .collect();
+        println!("En la iteracion {} se escoge {}", i, keep);
+    }
+    for ch in oxigen_vec[0].chars() {
+        if ch == '1' {
+            oxigen = 2 * oxigen + 1;
+        } else {
+            oxigen = 2 * oxigen;
+        }
+    }
+    for i in 0..bit_len {
+        if co2_vec.len() == 1 {
+            break;
+        }
+        let ones = co2_vec
+            .iter()
+            .filter(|s| s.chars().nth(i) == Some('1'))
+            .count();
+        let keep = if ones * 2 >= co2_vec.len() { '0' } else { '1' };
+        co2_vec = co2_vec
+            .into_iter()
+            .filter(|s| s.chars().nth(i) == Some(keep))
+            .collect();
+    }
+    for ch in co2_vec[0].chars() {
+        if ch == '1' {
+            co2 = 2 * co2 + 1;
+        } else {
+            co2 = 2 * co2;
+        }
+    }
+    println!("{}, {}", oxigen_vec[0], oxigen);
+    println!("{}, {}", co2_vec[0], co2);
+    let result2 = oxigen * co2;
+
     println!("Result 1: {}", result1);
     println!("Result 2: {}", result2);
     Ok(())
@@ -47,7 +107,6 @@ fn read_input() -> io::Result<Vec<String>> {
     let mut content = Vec::new();
     for line in reader.lines() {
         let line = line?;
-        println!("{}", line);
         content.push(line);
     }
     Ok(content)
